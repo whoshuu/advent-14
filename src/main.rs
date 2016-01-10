05178253aafbbe53880b6e42c6587bed5138dd4b
Huu Nguyen <whoshuu@gmail.com>
2016-01-10 14:49:49 -0500

    Remove example from main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 extern crate advent_14;
 
 fn main() {
-    println!("Comet distance: {}", advent_14::total_distance(14, 10, 127, 1000));
-    println!("Dancer distance: {}", advent_14::total_distance(16, 11, 162, 1000));
 }
